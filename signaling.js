0198abe8555507e5f8a9f96c2fb08e29729e52eb
Minh Minh <minhminh9201@gmail.com>
2021-08-20 22:34:24 +0700

    push follow example heroku

--- signaling.js
+++ signaling.js
@@ -1,8 +1,40 @@
+// 'use strict';
+
+// const express = require('express');
+// const socketIO = require('socket.io');
+// const RoomSignalingManager = require('./signaling_socket/socket_manager')
+
+// const PORT = process.env.PORT || 3000;
+// const INDEX = '/index.html';
+
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// const io = socketIO(server);
+
+// //Setup manager in app
+// io.signalingManager = new RoomSignalingManager(io)
+
+// io.sockets.on("connection", (socket) => {
+
+//   console.log(`On Connection ${socket.id}`)
+//   socket.io = io;
+//   _intializeSocket(socket)
+
+//   socket.on("disconnect", () => {
+//     console.log(`On Disconnect ${socket.id}`)
+//   })
+// })
+
+// function _intializeSocket (socket) {
+//   new (require('./signaling_socket/socket_control'))(socket) 
+// }
+
 'use strict';
 
 const express = require('express');
 const socketIO = require('socket.io');
-const RoomSignalingManager = require('./signaling_socket/socket_manager')
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
@@ -13,20 +45,7 @@ const server = express()
 
 const io = socketIO(server);
 
-//Setup manager in app
-io.signalingManager = new RoomSignalingManager(io)
-
-io.sockets.on("connection", (socket) => {
-
-  console.log(`On Connection ${socket.id}`)
-  socket.io = io;
-  _intializeSocket(socket)
-
-  socket.on("disconnect", () => {
-    console.log(`On Disconnect ${socket.id}`)
-  })
-})
-
-function _intializeSocket (socket) {
-  new (require('./signaling_socket/socket_control'))(socket) 
-}
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
