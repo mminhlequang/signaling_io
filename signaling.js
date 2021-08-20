7ff0c792aa94a0300a11f90bf992e262fb61c71a
Minh Minh <minhminh9201@gmail.com>
2021-08-20 23:25:24 +0700

    Push

---
+++ signaling.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const RoomSignalingManager = require('./signaling_socket/socket_manager')
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+//Setup manager in app
+io.signalingManager = new RoomSignalingManager(io)
+
+let total = 0
+io.sockets.on("connection", (socket) => {
+
+  console.log(`On Connection ${socket.id}`)
+  total = total + 1;
+  io.emit('signaling_logs', total)
+  socket.io = io;
+  _intializeSocket(socket)
+
+  socket.on("disconnect", () => {
+    console.log(`On Disconnect ${socket.id}`)
+    total = total - 1;
+    io.emit('signaling_logs', total)
+  })
+})
+
+function _intializeSocket (socket) {
+  new (require('./signaling_socket/socket_control'))(socket) 
+}
---
+++ signaling_socket/constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+  onJoin: 'signaling_join',
+  onOffer: 'signaling_offer', 
+  onAnswer: 'signaling_answer', 
+  onCandidate: 'signaling_candidate', 
+  onLeave: 'signaling_leave',
+  onBye: 'signaling_bye',
+
+  emitInfo: 'signaling_info',
+  emitJoin: 'signaling_join',
+  emitOffer: 'signaling_offer', 
+  emitAnswer: 'signaling_answer', 
+  emitCandidate: 'signaling_candidate', 
+  emitLeave: 'signaling_leave',
+  emitBye: 'signaling_bye'
+}
